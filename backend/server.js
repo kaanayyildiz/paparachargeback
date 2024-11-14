d5029fe5101eb14d3824942a9cecc33f81c738fa
Kaan Ayyıldız <ykaanayyildiz@gmail.com>
2024-11-14 09:39:31 +0300

    update

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,31 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
-import path from "path";
-import authRoute from "./routes/auth.route.js";
-import { connectDB } from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
+import {connectDB} from "./db/connectDB.js";
 
 dotenv.config();
-connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-const __dirname = path.resolve()
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-})
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
+app.use(express.json({ limit: "5mb" })); // to parse req.body
+// limit shouldn't be too high to prevent DOS
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+
+app.use(cookieParser());
+
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
 
-app.use("/api/auth", authRoute);
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
+	connectDB();
 });
