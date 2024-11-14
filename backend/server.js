814bd742ec6318e46a6999c3d70cb8d33c8b1f70
Kaan Ayyıldız <ykaanayyildiz@gmail.com>
2024-11-14 09:27:38 +0300

    update

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import path from "path";
 import authRoute from "./routes/auth.route.js";
 import { connectDB } from "./db/connectDB.js";
 
@@ -9,6 +9,14 @@ connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve()
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+})
+
+}
 
 app.use("/api/auth", authRoute);
 
